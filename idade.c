d7e2d2f985699ec711e7d55cb7452fae42647388
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-11-24 16:32:14 -0300

    Add files via upload

--- idade.c
+++ idade.c
@@ -5,10 +5,10 @@ int main(){
 
     int idade;
 
-    printf('Qual a sua idade?:');
+    printf("Qual a sua idade?:");
     scanf("%d", &idade);
-    printf('A idade informada foi: %d \n', idade);
+    printf("A idade informada foi: %d \n", idade);
 
-    system('pause');
+    system("pause");
     return 0;
 }
