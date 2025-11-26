c209451b0e692b6a18a798720841f808c340f8d7
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-11-26 15:44:09 -0300

    Add files via upload

---
+++ treinoif02.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int age1, age2;
+
+    printf("INFORME A PRIMEIRA IDADE:");
+    fflush(stdin);
+    scanf("%d", &age1);
+    printf("INFORME A SEGUNDA IDADE:");
+    fflush(stdin);
+    scanf("%d", &age2);
+
+    printf("\n");
+
+    if(age1 > age2){
+        printf("A MAIOR IDADE É %d .\n", age1);
+    }else if(age1 == age2){
+         printf("AS IDADES SÃO AS MESMAS .\n");
+    }else{
+        printf("A MAIOR IDADE É %d .\n", age2);
+    }
+
+    system("pause");
+    return 0;
+}
---
+++ treinoif05.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+
+    int idade;
+
+    printf("INFORME UMA IDADE: ");
+    scanf(" %d", &idade);
+
+    if(idade >= 15 && idade <=18){
+        printf("a idade esta entre 15 e 18 ");
+    }else{
+        printf("não esta entre 15 e 18 ");
+    }
+    
+    system("pause");
+    return 0;
+}
