50d8d5695ffa8008bfa0ffbe753ff1016308681b
André Dias <57869108+Andredev-dias@users.noreply.github.com>
2025-11-27 16:16:59 -0300

    Add files via upload

---
+++ func02.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int quad(int x);
+
+int main(){
+
+    setlocale(LC_ALL, "portuguese");
+
+    int num, res;
+
+    printf("Digite um número: ");
+    scanf(" %d", &num);
+
+    res = quad(num);
+    printf("O QUADRADO DE %d é: %d \n", num, res);
+
+    system("pause");
+    return 0;
+}
+
+int quad(int x){
+    int quadrado;
+
+    quadrado = x*x;
+    return quadrado;
+}
